e59244900fdc7ca3b75ba6ccfd8724c0c8e62782
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-16 14:53:53 +0500

    Rataion when Theta2 is 90 degree

--- RotationMatrix2DoF.py
+++ RotationMatrix2DoF.py
@@ -13,13 +13,12 @@ R1_2 = [[np.cos(Theta2), -np.sin(Theta2),0],[np.sin(Theta2) ,np.cos(Theta2),0 ],
 R0_2 = np.dot(R0_1,R1_2)
 print(R0_2)
 
-
 #Finding Roataion when Theta2 is set to 90 degree
 print("\n")
 Theta1 = 0                          #Initial angle 
-Theta2 = 0                          #initial angle
-Theta1 = (Theta1/180)*np.pi         #
-Theta2 = (Theta2/180)*np.pi
+Theta2 = 90                         #initial angle
+Theta1 = (Theta1/180)*np.pi         #Converting Theta1 into Radian
+Theta2 = (Theta2/180)*np.pi         #Converting Theta2 into Radian
 
 R0_1 = [[np.cos(Theta1), -np.sin(Theta1),0],[np.sin(Theta1) ,np.cos(Theta1),0 ],[0,0,1]]
 R1_2 = [[np.cos(Theta2), -np.sin(Theta2),0],[np.sin(Theta2) ,np.cos(Theta2),0 ],[0,0,1]]
